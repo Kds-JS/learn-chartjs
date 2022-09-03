f1e243c112e58f8bc59281a0d51c3f8d6e10172e
kds <kdskoudous98@gmail.com>
2022-09-03 08:53:38 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chart Js Hello</h1>
+    </div>
+  );
+}
+
+export default App;
